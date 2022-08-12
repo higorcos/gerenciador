f50e497a5aeabba199cf998a4b1223b4ab481d57
higorcos <higorcosta2001@outlook.com>
2022-08-12 03:33:37 -0300

    link

--- src/variables/buttonHome.js
+++ src/variables/buttonHome.js
@@ -1,6 +1,6 @@
 export  const buttons = [
  
- {name: `Botões`, link: `/botoes/criar`, img: `grafico`}
+ {name: `Botões`, link: `/botoes/mostrar`, img: `grafico`}
 ,{name: `Autorizar Usuário`, link: `#/user`, img: `users`}
 ,{name: `Upload de documento`, link: `#upload`, img: `upload`}
 ,{name: `Competências`, link: `http://ec2-54-207-215-247.sa-east-1.compute.amazonaws.com:8000/admin/competencias`, img: `comp`}
